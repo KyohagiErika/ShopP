23d0579cc29abf03f398ca7779adea8de796efd7
Le Thanh Long <favefrong2003@gmail.com>
2022-08-04 17:22:47 +0700

    ShopP Init

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
-exports.default = routes;
--- dist/utils/shopp.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-class ShopPConfig {
-}
-exports.default = ShopPConfig;
-ShopPConfig.PORT = parseInt(process.env.PORT || '3000');
