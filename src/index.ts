57c62f506fbb6e54174475b3a6c6a845fccceaee
Lê Thành Long <48479168+KyohagiErika@users.noreply.github.com>
2022-09-11 09:18:26 +0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -15,33 +15,25 @@ app.use(cors());
 app.use(helmet());
 app.use(routes);
 const server = http.createServer(app);
-server.listen(ShopPConfig.PORT, () => {
-  console.log(
-    chalk.bold(
-      chalk.magenta(`Server is listenning at port ${ShopPConfig.PORT}!`)
-    )
-  );
-  server.emit('ok');
-});
 
-// ShopPDataSource.initialize()
-//   .then(source => {
-//     server.listen(ShopPConfig.PORT, () => {
-//       console.log(
-//         chalk.bold(
-//           chalk.magenta(`Server is listenning at port ${ShopPConfig.PORT}!`)
-//         )
-//       );
-//       server.emit('ok');
-//     });
-//   })
-//   .catch(err => {
-//     console.error(
-//       chalk.red('There are some errors while initialzing data source!')
-//     );
-//     console.error('Detail:');
-//     console.log(err);
-//     process.exit(1);
-//   });
+ShopPDataSource.initialize()
+  .then(source => {
+    server.listen(ShopPConfig.PORT, () => {
+      console.log(
+        chalk.bold(
+          chalk.magenta(`Server is listenning at port ${ShopPConfig.PORT}!`)
+        )
+      );
+      server.emit('ok');
+    });
+  })
+  .catch(err => {
+    console.error(
+      chalk.red('There are some errors while initialzing data source!')
+    );
+    console.error('Detail:');
+    console.log(err);
+    process.exit(1);
+  });
 
 export default server;
