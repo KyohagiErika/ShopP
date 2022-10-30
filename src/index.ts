15e7d38df6c52e8e21e66c2785058cff4119078d
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-10-30 20:31:54 +0700

    Delete helmet

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
 app.use(routes);
 
 const server = http.createServer(app);
