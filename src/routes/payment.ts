418c3ce7ebb6e321b70ab2ea0bc397ac051fa815
linhlinh38 <99740575+linhlinh38@users.noreply.github.com>
2022-12-05 19:52:35 +0700

    swagger-payment

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -6,18 +6,94 @@ import { RoleEnum } from '../utils/shopp.enum';
 
 const routes = Router();
 
+/**
+ * @swagger
+ * /payment/list-all:
+ *  get:
+ *   tags:
+ *    - Payment
+ *   security:
+ *    - bearerAuth: []
+ *   summary: List all payments (Customer)
+ *   description: List all payments (Customer)
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/PaymentListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/list-all',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   PaymentMiddleware.listAll
 );
 
+/**
+ * @swagger
+ * /payment/{id}:
+ *  get:
+ *   tags:
+ *    - Payment
+ *   security:
+ *    - bearerAuth: []
+ *   summary: Get one payment (Customer)
+ *   description: Get one payment (Customer)
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: integer
+ *       format: int32
+ *      required: true
+ *      description: id of the payment
+ *      example: 1
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *         $ref: '#/components/schemas/PaymentResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/:id([0-9]+)',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   PaymentMiddleware.getOneById
 );
 
+/**
+ * @swagger
+ * /payment/new:
+ *  post:
+ *   tags:
+ *    - Payment
+ *   security:
+ *    - bearerAuth: []
+ *   summary: Create a new payment (admin)
+ *   description: Create a new payment (admin)
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/PaymentRequest'
+ *   responses:
+ *    200:
+ *     $ref: '#/components/responses/200OK'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.post(
   '/new',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.ADMIN)],
