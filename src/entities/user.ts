22996b8cf736b0fcaeaf33842c4db40f7c41dd52
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-12-10 21:00:41 +0700

    Update user.ts

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -13,7 +13,7 @@ import {
 import bcrypt from 'bcryptjs';
 import { UserRole } from './userRole';
 import { Customer } from './customer';
-import { StatusEnum } from '../utils/shopp.enum'
+import { StatusEnum } from '../utils/shopp.enum';
 import { Shop } from './shop';
 import { Event } from './event';
 
