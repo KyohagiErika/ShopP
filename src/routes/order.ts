de6b92fef0425570260af157e78357e1f106c063
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-12-14 23:05:50 +0700

    Add order swagger

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -5,67 +5,323 @@ import OrderMiddleware from '../middlewares/order';
 import { RoleEnum } from '../utils/shopp.enum';
 
 const routes = Router();
-
+/**
+ * @swagger
+ * /order/customer:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Order (Checking, Confirmed, Packaging) For Customer
+ *   description: View Order (Checking, Confirmed, Packaging) For Customer
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/CustomerOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/customer',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   OrderMiddleware.viewOrderForCustomer
 );
 
+/**
+ * @swagger
+ * /order/customer-deliver:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Delivering Order For Customer
+ *   description: View Delivering Order For Customer
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/CustomerOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/customer-deliver',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   OrderMiddleware.viewOrderDeliverForCus
 );
 
+/**
+ * @swagger
+ * /order/customer-history:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Order History (Delivered) For Customer
+ *   description: View Order History (Delivered) For Customer
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/CustomerOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/customer-history',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   OrderMiddleware.viewHistoryForCus
 );
 
+/**
+ * @swagger
+ * /order/customer-cancel:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Canceled Order For Customer
+ *   description: View Canceled Order For Customer
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/CustomerOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/customer-cancel',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   OrderMiddleware.viewCancelOrderForCus
 );
 
+/**
+ * @swagger
+ * /order/shop:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Checking Order For Shop
+ *   description: View Checking Order For Shop
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ShopOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/shop',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.SHOP)],
   OrderMiddleware.viewOrderForShop
 );
 
+/**
+ * @swagger
+ * /order/shop-confirm:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Confirmed Order For Shop
+ *   description: View Confirmed Order For Shop
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ShopOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/shop-confirm',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.SHOP)],
   OrderMiddleware.viewConfirmOrderForShop
 );
 
+/**
+ * @swagger
+ * /order/shop-deliver:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Delivering Order For Shop
+ *   description: View Delivering Order For Shop
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ShopOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/shop-deliver',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.SHOP)],
   OrderMiddleware.viewOrderDeliverForShop
 );
 
+/**
+ * @swagger
+ * /order/shop-history:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: View Order History (Delivered) For Shop
+ *   description: View Order History (Delivered) For Shop
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ShopOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/shop-history',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.SHOP)],
   OrderMiddleware.viewHistoryForShop
 );
 
+/**
+ * @swagger
+ * /order/new:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: Post new order (Shop)
+ *   description: Post new order (Shop)
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/OrderRequest'
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ShopOrderListResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.post(
   '/new',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   OrderMiddleware.postNew
 );
 
+/**
+ * @swagger
+ * /order/cancel-order/{id}:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: Cancel Order (Customer)
+ *   description: Cancel Order (Customer)
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *      description: id of the order
+ *      example: 1a2b3c4d-5e6f-7g8h-9i0j-1k2l3m4n5o6p
+ *   responses:
+ *    200:
+ *     $ref: '#/components/responses/200Ok'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.post(
   '/cancel-order/:id',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
   OrderMiddleware.cancelOrder
 );
 
+/**
+ * @swagger
+ * /order/edit-status/{id}:
+ *  get:
+ *   tags:
+ *    - Order
+ *   security:
+ *    - bearerAuth: []
+ *   summary: Edit Order Status (Shop)
+ *   description: Edit Order Status (Shop)
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *      description: id of the order
+ *      example: 1a2b3c4d-5e6f-7g8h-9i0j-1k2l3m4n5o6p
+ *   responses:
+ *    200:
+ *     $ref: '#/components/responses/200Ok'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.post(
   '/edit-status/:id',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.SHOP)],
--- src/routes/orderProduct.ts
+++ src/routes/orderProduct.ts
@@ -5,6 +5,36 @@ import OrderProductMiddleware from '../middlewares/orderProduct';
 import { RoleEnum } from '../utils/shopp.enum';
 
 const routes = Router();
+/**
+ * @swagger
+ * /order-product/view-order-product/{id}:
+ *  get:
+ *   tags:
+ *    - Order Product
+ *   security:
+ *    - bearerAuth: []
+ *   summary: Get order product by id
+ *   description: Get order product by id
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *      description: id of the order product
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/OrderProductResponse'
+ *    400:
+ *     $ref: '#/components/responses/400BadRequest'
+ *    401:
+ *     $ref: '#/components/responses/401Unauthorized'
+ */
 routes.get(
   '/view-order-product/:id',
   [AuthMiddleware.checkJwt, checkRole(RoleEnum.CUSTOMER)],
